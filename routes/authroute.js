ae19f42a99b3f1be14afc1a89e26017354d67604
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-06-21 09:34:00 -0400

    Added IsTokenValid on backend

--- routes/authroute.js
+++ routes/authroute.js
@@ -16,6 +16,27 @@ function getExpiration() {
 
 const router = Router();
 
+// api/auth/isTokenValid
+router.get('/isTokenValid', verifyAuth, async (req, res) => {
+  try {
+    // TODO Check in DB if the user is valid as an additional check
+    if (req.id) {
+      res.status(200).json({
+        valid: true,
+        status: 200,
+        message: "Token is valid"
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      valid: false,
+      status: 400,
+      error: `Token is invalid`,
+      database_message: error.message,
+    })
+  }
+})
+
 // api/auth/signup
 //working
 router.post('/signup', async (req, res) => {
